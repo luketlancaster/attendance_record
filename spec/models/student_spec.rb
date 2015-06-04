d34e5ac7c444cbb45d806cd0ae4077b9140b9c3d
Luke Lancaster <luketlancaster@gmail.com>
2015-06-04 11:51:10 -0500

    Initial validation for student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+  it "should have a working factory" do
+    Fabricate.build(:student).should be_valid
+  end
 end
